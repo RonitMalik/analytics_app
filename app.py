83089f320f796435d1f48bcb4ecada9a9a788f35
Ronit Malik <46568826+RonitMalik@users.noreply.github.com>
2022-06-29 23:52:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -38,12 +38,15 @@ def main():
         st.dataframe(df.head())
         profile = ProfileReport(df)
         if st.button("Generate Report"):
-            st_profile_report(profile)
             lottie_hello1 = load_lottieurl('https://assets2.lottiefiles.com/packages/lf20_oi0plzot.json')
             st_lottie(
             lottie_hello1,
             speed=1,
+            height=50,
+            width=50
             )
+            st_profile_report(profile)
+
 
 if __name__=='__main__':
     main()
