256679af5443f4804e6347669d1ed4036dad4416
Ronit Malik <46568826+RonitMalik@users.noreply.github.com>
2022-06-29 21:52:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pandas as pd
 import codecs
 from pandas_profiling import ProfileReport
+import time
+from streamlit_lottie import st_lottie
+import json
+import requests
+
 
 import streamlit.components.v1 as components
 from streamlit_pandas_profiling import st_profile_report
@@ -9,10 +14,26 @@ from streamlit_pandas_profiling import st_profile_report
 st.cache(allow_output_mutation=True)
 def main():
 
+
+    def load_lottieurl(url: str):
+        r = requests.get(url)
+        if r.status_code != 200:
+            return None
+        return r.json()
+
+    lottie_hello = load_lottieurl('https://assets6.lottiefiles.com/packages/lf20_w9wl8mlm.json')
+    st_lottie(
+    lottie_hello,
+    speed=1,
+    )
+
+
     st.title('''
     **Descriptive Analytics Tool**''')
     st.markdown("This tool used for descriptive analysis for any CSV file that you upload")
     data_file = st.file_uploader("Upload Your CSV", type=['CSV'])
+
+
     if data_file is not None:
         df = pd.read_csv(data_file)
         st.dataframe(df.head())
