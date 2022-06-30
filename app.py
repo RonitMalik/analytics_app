351404e06e0b069a5cf3566bab8fb7a8449bf01d
Ronit Malik <46568826+RonitMalik@users.noreply.github.com>
2022-06-30 17:18:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -33,41 +33,24 @@ def main():
     menu = ['Profiling Report', 'Profiling Report Non-technical']
     choice = st.sidebar.selectbox("Choose your descriptive analytics tool", menu)
 
-    if choice == 'Profiling Report':
-        lottie_hello = load_lottieurl('https://assets6.lottiefiles.com/packages/lf20_w9wl8mlm.json')
-        st_lottie(
-        lottie_hello,
-        speed=1,
-        )
+    lottie_hello = load_lottieurl('https://assets6.lottiefiles.com/packages/lf20_hx7ddrx9.json')
+    st_lottie(
+    lottie_hello,
+    speed=1,
+    )
 
-        st.title('''
-        **Descriptive Analytics Tool**''')
-        st.markdown("This tool used for descriptive analysis for any CSV file that you upload")
-        data_file = st.file_uploader("Upload Your CSV", type=['CSV'])
+    st.title('''
+    **Descriptive Analytics Tool**''')
+    st.markdown("This tool used for descriptive analysis for any CSV file that you upload")
+    data_file = st.file_uploader("Upload Your CSV", type=['CSV'])
 
-        if data_file is not None:
-            df = pd.read_csv(data_file)
-            st.dataframe(df.head())
-            profile = ProfileReport(df)
-            if st.button("Generate Report"):
-                st_profile_report(profile)
+    if data_file is not None:
+        df = pd.read_csv(data_file)
+        st.dataframe(df.head())
+        profile = ProfileReport(df)
+        if st.button("Generate Report"):
+            st_profile_report(profile)
 
-    elif choice == 'Profiling Report Non-technical':
 
-        lottie_hello_1 = load_lottieurl('https://assets6.lottiefiles.com/packages/lf20_hx7ddrx9.json')
-        st_lottie(
-        lottie_hello_1,
-        speed=1,
-        )
-
-        st.title('''
-        **Descriptive Analytics Tool**''')
-
-        data_file = st.file_uploader("Upload Your CSV", type=['CSV'])
-        if data_file is not None:
-            df = pd.read_csv(data_file)
-            st.dataframe(df.head())
-            if st.button("Generate sweetviz Report"):
-                st_display_sweetviz(df)
 if __name__=='__main__':
     main()
